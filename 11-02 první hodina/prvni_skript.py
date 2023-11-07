ed25dd0ef26ed7cdccea8ed91c0a49def0d59dd9
Trohat <petr.sed@centrum.cz>
2023-11-07 19:47:31 +0100

    Druhá lekce

---
+++ 11-02 první hodina/prvni_skript.py
@@ -0,0 +1,13 @@
+
+print("Ahoj světe")
+
+print("Jak se dneska máte")
+
+
+# tohle je komentář
+
+"""
+ahoj tohle je string ale zároveň
+komentář
+(víceřádkový)
+"""
---
+++ 11-07 proměnné a datové typy/druha_mocnina.py
@@ -0,0 +1,14 @@
+
+print("Tenhle program vypočítá druhou mocninu zadaného čísla...")
+
+cislo = int(input("Zadej číslo k výpočtu: "))
+
+vysledek = cislo * cislo 
+
+print("Výsledek mocniny je", vysledek)
+
+
+
+
+
+
