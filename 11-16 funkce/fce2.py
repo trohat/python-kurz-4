8cc8b7e65a813e037b23ef5c6cee0699835bcf49
Trohat <petr.sed@centrum.cz>
2023-11-21 20:26:20 +0100

    seznamy

--- 11-16 funkce/fce2.py
+++ 11-16 funkce/fce2.py
@@ -3,3 +3,10 @@ def scitani(cislo1, cislo2):
     vysledek = cislo1 + cislo2
     print(f"Výsledek je {vysledek}")
 
+def scitani2(cislo1, cislo2):
+    vysledek = cislo1 + cislo2
+    #TODO: vymazat - jen pro ladění programu
+    print(f"1 - Vynásobili sme {cislo1} a {cislo2} a vyšlo {vysledek}.")
+    return vysledek
+
+
---
+++ 11-21 seznamy/seznam.py
@@ -0,0 +1,13 @@
+
+cena1 = 50
+cena2 = 55
+cena3 = 62
+cena4 = 67
+cena5 = 66
+cena6 = 62
+cena7 = 54
+cena8 = 45
+cena9 = 46
+cena10 = 52
+
+cena228 = "nechci to tak dělat"
